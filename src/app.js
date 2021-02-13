417b08a19e7e32170243326d4fd2b4708847187c
ves14 <ves14@hi.is>
2021-02-13 21:18:55 +0000

    i don't know anymore

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import join,__dirname from 'path';
+import url from ''
 
 dotenv.config();
 
 const {
-  PORT: port = 3000,
+  PORT: port = 3070,
 } = process.env;
 
 const app = express();
 
 // TODO setja upp rest af virkni!
+//Ná í index.ejs
+app.set('view engine','ejs')
+app.get('/', (req, res) => {
+    res.render('index');
+    
+})
 
 // Verðum að setja bara *port* svo virki á heroku
 app.listen(port, () => {
--- src/registration.js
+++ src/registration.js
@@ -1,3 +1,23 @@
 import express from 'express';
 
 // TODO skráningar virkni
+//Make a router for connection
+const router = express.Router;
+
+const connectionString = 'postgres://vef2-2021:123@localhost/vef2-2021';
+const pool = new pg.Pool({ connectionString });
+
+async function main() {
+    
+    const client = await pool.connect();
+  
+    try {
+      const result = await client.query('SELECT * FROM people');
+      console.log('rows :>> ', result.rows);
+    } catch (e) {
+      console.error('Error selecting', e);
+    } finally {
+      client.release();
+    }
+  
+    await pool.end();
